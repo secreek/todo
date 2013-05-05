b4817b1ff4e04f73afda7fb2f7a9c3ca37639f4e
hit9 <nz2324@126.com>
2013-05-05 22:00:10 +0800

    remove object tag

--- todo.py
+++ todo.py
@@ -27,34 +27,17 @@ class Task(object):
         self.done = done
 
 
-class Tag(object):
-    """
-    Tag object.
-    Tags split tasks list like milestones.
-    One tag looks like:
-      ----- The tasks above is before 12-04-05 ----
-    The string 'The....05' is the tag's content, it should be unique.
-
-    name        str     tag's name, should be unique in all tags
-    """
-
-    def __init__(self, name):
-        self.name = name
-
-
 class TodoLexer(object):
     """
     Lexer for Todo format string.
     Tokens
       ID        e.g. '1.'
-      TAG       e.g. '---- SampleTag ----'
       DONE      e.g. '(x)'
       TASK      e.g. 'This is a task'
     """
 
     tokens = (
         "ID",
-        "TAG",
         "DONE",
         "TASK",
     )
@@ -64,12 +47,6 @@ class TodoLexer(object):
     def t_ID(self, t):
         r'\d+\.([uU]|[lL]|[uU][lL]|[lL][uU])?'
         t.value = int(t.value[:-1])
-        return t
-
-    def t_TAG(self, t):
-        r'-+(.*)-+'
-        t.value = ''.join(i for i in t.value if i != '-')
-        t.value = t.value.strip()
         return t
 
     def t_DONE(self, t):
@@ -126,15 +103,15 @@ class TodoParser(object):
 
     def p_translation_unit(self, p):
         """
-        translation_unit : translate_single_line
-                         | translation_unit translate_single_line
+        translation_unit : translate_task
+                         | translation_unit translate_task
                          |
         """
         pass
 
     def p_translation_task(self, p):
         """
-        translate_single_line : ID DONE TASK
+        translate_task : ID DONE TASK
                               | ID TASK
         """
         if len(p) == 4:
@@ -145,13 +122,6 @@ class TodoParser(object):
             content = p[2]
         task = Task(p[1], content, done)
         self.lst.append(task)
-
-    def p_translation_tag(self, p):
-        """
-        translate_single_line : TAG
-        """
-        tag = Tag(p[1])
-        self.lst.append(tag)
 
     def __init__(self):
         self.parser = yacc.yacc(module=self, debug=0, write_tables=0)
@@ -181,12 +151,6 @@ class TodoGenerator(object):
     def g_task(self, v):
         return v
 
-    def g_tag(self, v):
-        return '---- ' + v + ' ----'
-
-    def gen_tag(self, tag):
-        return self.g_tag(tag.name)
-
     def gen_task(self, task):
         lst = []
         lst.append(self.g_id(task.id))
@@ -197,9 +161,7 @@ class TodoGenerator(object):
     def generate(self, lst):
         re = []
         for i in lst:
-            if isinstance(i, Tag):
-                re.append(self.gen_tag(i))
-            elif isinstance(i, Task):
+            if isinstance(i, Task):
                 re.append(self.gen_task(i))
             else:
                 raise SyntaxError('Not support type: ' + type(i))
@@ -218,3 +180,5 @@ for i in lst:
         print i.id, i.content, i.done
 
 print generator.generate(lst)
+
+print lexer.test(open("todo.txt").read())
