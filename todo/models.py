d6eb1dcc4ba69c86e40e7ad662c4e4573427475d
hit9 <nz2324@126.com>
2013-05-14 13:55:29 +0800

    Rebuilt models and parser

--- todo/models.py
+++ todo/models.py
@@ -1,188 +1,50 @@
 # coding=utf8
-
-import json
-import requests
+# _____      _________
+# __  /____________  /_____
+# _  __/  __ \  __  /_  __ \
+# / /_ / /_/ / /_/ / / /_/ /
+# \__/ \____/\__,_/  \____/
+#
+# Todo application in the command line, with readable storage.
+# Authors: https://github.com/secreek
+# Home: https://github.com/secreek/todo
+# Email: nz2324@126.com
+# License: MIT
+"""
+  Models in this application, Task and Todo,
+see theirs docs for help.
+"""
 
 
 class Task(object):
     """
-    A task looks like
+      A task looks like::
 
-      1. [x]  Go shopping
+          - [x] Go shopping
 
-    if this task has been done, use '[x]' to mark it.
-    else, leave there blank.
+      Use '[x]' to mark a task done.
 
-    attributes
-      id        int
-      content   str
-      done      bool
+      attributes
+        content   str  (required)
+        done      bool (optional, default: False)
     """
 
-    def __init__(self, task_id, content, done=False):
-        self.id = task_id
+    def __init__(self, content, done=False):
         self.content = content
         self.done = done
 
 
-class TaskNotFound(Exception):
-    """
-    If the asked task not found in given todo, this error will be raised
-    """
-    pass
-
-
 class Todo(object):
     """
-    A todo is made up of tasks.
+      A todo is made up of tasks.
 
-    attributes
-      name    str     todo's name, should be unique in all your todos in your os.
-      tasks   list    tasks in this todo, each of them is an instance of Task
+      attributes
+        name    str     (optional, default: '')todo's name, should be unique
+      in all your todos in your os.
+        tasks   list    (optional, default: [])tasks in this todo, each of
+      them is an instance of Task
     """
 
-    def __init__(self, name="", tasks=[]):
-        # We set default value of argument 'name' as "",
-        # because the name is optional
-        self.name = name
-        self.tasks = tasks
-
-    def next_id(self):
-        """
-        Generate the next id should be.
-
-        For the id of one task is an integer, the id(s) of a todo object will be made up of
-        many integers. We find the max of them, and plus one to it as the next id for new task
-        """
-
-        ids = [task.id for task in self.tasks]
-        max_id = max(ids) if ids else 0  # method `max` broken with empty list
-        return (max_id + 1)
-
-    def new_task(self, content, done=False):
-        """
-        Append a new task to todo.
-
-        parameters:
-          content       the content of task
-          done          if the task done(default: False)
-        """
-        task = Task(self.next_id(), content, done)
-        return self.tasks.append(task)
-
-    def get_task(self, task_id):
-        """
-        Get task by its id
-        """
-        for task in self.tasks:
-            if task.id == task_id:
-                return task
-        # Not found
-        raise TaskNotFound
-
-    def remove_task(self, task_id):
-        """
-        Remove task by its id
-        """
-        task = self.get_task(task_id)
-        return self.tasks.remove(task)
-
-    def clear(self):
-        """
-        Clear tasks list!
-        """
-        self.tasks = []
-
-
-class Github(object):
-    """
-    Github object to auth user, edit gist .etc
-
-    e.g. ::
-        gh = Github()
-        gh.login(token='xxxx')
-        gh.edit_gist()
-
-    And about the authorize::
-        gh = Github()
-        token = gh.authorize(login, password)  # authorize itself and login
-        gh.login(token=token)
-        gh.edit_gist()  # do things..
-    """
-
-    # The client_secret should not be shared in principle.
-    # Otherwise a proxy script running all days around will be needed.
-    # DONT DO THINGS STUPID.
-    # The client_secret can be reset. Also, this application matsers nothing.
-    # Thanks.
-
-    client_id = "141eec25d89ad52c380b"
-    client_secret = "5bf0c8d82a753e4efbdf045400989f8fa93843d8"
-    note_url = "https://github.com/secreek/todo"
-    note = "Cli todo tool with readable storage."
-    scopes = ["user", "gist"]
-
-    def __init__(self):
-        """
-        Init an instance of Github. New an empty session.
-        """
-        self.session = requests.Session()  # init a session
-
-    def authorize(self, login, password):
-        """
-        Fetch access_token from github.com using username & password.
-        return the response object
-        """
-        self.session.auth = (login, password)
-        data = dict(
-            client_id=self.client_id,
-            client_secret=self.client_secret,
-            note=self.note,
-            note_url=self.note_url,
-            scopes=self.scopes
-        )
-
-        data_json = json.dumps(data)
-        headers = {'content-type': 'application/json'}
-        return self.session.post("https://api.github.com/authorizations", data=data_json, headers=headers)
-
-    def login(self, token):
-        """
-        Login to github with token
-        """
-        self.session.headers.update({'Authorization': 'token ' + token})
-
-    def edit_gist(self, gist_id, files={}, description=""):
-        """
-        Edit a gist, require auth.
-
-        parameters
-          gist_id       str      gist's id
-          files         dict     {file_name:{"content":"xxxx", "filename":"xxxxx"}}
-          description   str      gist's description
-
-        return response
-        """
-        data = dict(
-            files=files,
-            description=description
-        )
-        response = self.session.patch("https://api.github.com/gists/" + gist_id, data=json.dumps(data))
-        return response
-
-    def get_gist(self, gist_id):
-        """
-        Fetch a single gist down, return response.
-
-        ::
-            resp = get_gist("xxx")
-            dct = resp.json()
-
-        To get some certain file's raw_url::
-            dct["files"]["filename"]["raw_url"]
-
-        To fetch content of gist's file::
-           r = requests.get(file_raw_url)
-           print r.text
-        """
-        return self.session.get("https://api.github.com/gists/" + gist_id)
+    def __init__(self, name=None, tasks=None):
+        self.name = '' if name is None else name
+        self.tasks = [] if tasks is None else tasks
--- todo/parser.py
+++ todo/parser.py
@@ -1,50 +1,82 @@
 # coding=utf8
+# _____      _________
+# __  /____________  /_____
+# _  __/  __ \  __  /_  __ \
+# / /_ / /_/ / /_/ / / /_/ /
+# \__/ \____/\__,_/  \____/
+#
+# Todo application in the command line, with readable storage.
+# Authors: https://github.com/secreek
+# Home: https://github.com/secreek/todo
+# Email: nz2324@126.com
+# License: MIT
 
 """
-Parser to parse todo format string to todo object.
+  Parse todo.txt to python's object <Todo>.
+
+  To parse string to <Todo instance>::
+
+      parser.parse(string)  # return <Todo instance>
+
+  What the todo format is? A todo is made up of name and tasks.
+  A sample looks like::
+
+    Todo's name
+    ------------
+
+    - [x] Here is todo's task
+
+    -     And another task
+
+  As you see, the format of todo string is obvious and readable. The string
+above maps to a <Todo> instance, like a list.
+
 """
 
-from models import Task
 from models import Todo
+from models import Task
 
-import re
 from ply import lex
 from ply import yacc
 
 
-class Lexer(object):
+class TodoSyntaxError(Exception):
     """
-    Lexer for todo format string
+      Raised when syntax errors occurred.
+    """
+    pass
 
-    Tokens
-      ID        e.g. '1.'
-      DONE      e.g. '[x]'
-      TASK      e.g. 'This is a sample task'
+
+class Lexer(object):  # really a simple parser, ha?
+    """
+      Lexer for todo.txt
+
+      Tokens
+        TASK    => Task     e.g. '- [x] task..'
+        NAME    => str      e.g. 'TodoName\n-------'
+
+      To use the lexer::
+          lexer = Lexer()
     """
 
-    tokens = (
-        "ID",
-        "DONE",
-        "TASK",
-    )
+    tokens = ("TASK", "NAME")
 
-    t_ignore = "\x20\x09"  # space & tab
+    t_ignore = " \t"  # ignore spaces and tabs
 
     def __init__(self):
         # the lexer built with instance
         self.lexer = lex.lex(module=self)
 
-    def t_ID(self, t):
-        r'\d+\.([uU]|[lL]|[uU][lL]|[lL][uU])?'
-        t.value = int(t.value[:-1])
-        return t
-
-    def t_DONE(self, t):
-        r'(\[x\])'
-        return t
-
     def t_TASK(self, t):
-        r'((?!\[x\])).+'
+        r'-\s+(?P<done>\[x\])?\s+(?P<content>.+)'
+        content = t.lexer.lexmatch.group('content')
+        done = True if t.lexer.lexmatch.group('done') else False
+        t.value = Task(content=content, done=done)
+        return t
+
+    def t_NAME(self, t):
+        r'(?P<name>[^\n]+)\n\s*-{3,}\s*\n*'
+        t.value = t.lexer.lexmatch.group('name')  # todo's name
         return t
 
     def t_newline(self, t):
@@ -52,88 +84,79 @@ class Lexer(object):
         t.lexer.lineno += len(t.value)
 
     def t_error(self, t):
-        raise SyntaxError("Illegal character '%s' at Line %d" % (t.value[0], t.lineno))
+        raise TodoSyntaxError(
+            "Illegal character '%s' at Line %d" % (t.value[0], t.lineno)
+        )
+
+    def test(self, data):
+        # test lexer
+        self.lexer.input(data)
+        while True:
+            tok = self.lexer.token()
+            if not tok:
+                break
+            print tok
 
 
 class Parser(object):
     """
-    Parser for todo format string, works with a lexer.
+      Parser for todo.txt, works with a lexer.
 
-    e.g.
-
-      parser = Parser()
-      parser.parse(string)  # return a todo instance
+      e.g.::
+          parser = Parser()
+          parser.parse(string)
     """
 
     tokens = Lexer.tokens
-
-    seperator_re = re.compile("\s*-+\s*")
-
-    def p_error(self, p):
-        if p:
-            raise SyntaxError("Character '%s' at line %d" % (p.value[0], p.lineno))
-        else:
-            raise SyntaxError("SyntaxError at EOF")
-
-    def p_start(self, p):
-        "start : translation_unit"
-        p[0] = self.todo
-
-    def p_translation_unit(self, p):
-        """
-        translation_unit : translate_task
-                         | translation_unit translate_task
-                         |
-        """
-        pass
-
-    def p_translation_task(self, p):
-        """
-        translate_task : ID DONE TASK
-                       | ID TASK
-        """
-        if len(p) == 4:
-            done = True
-            content = p[3]
-        elif len(p) == 3:
-            done = False
-            content = p[2]
-
-        task = Task(p[1], content, done)
-        # Note: this not new_task, but a task exist
-        # should be append to list directly
-        self.todo.tasks.append(task)
 
     def __init__(self):
         self.parser = yacc.yacc(module=self, debug=0, write_tables=0)
 
     def parse(self, data):
         """
-        Parse todo content to todo instance.
+          Parse todo.txt to todo instance.
 
-        parameter
-          data      str     todo format string
+          parameters
+            data        str     todo format string
+          return <Todo>
         """
-        data = data.strip()  # remove empty lines
+        self.todo = Todo()  # reset todo
+        return self.parser.parse(data)
 
-        lines = data.splitlines()
-        seperator_line = None
-
-        for lineno, line in enumerate(lines):
-            if self.seperator_re.match(line):
-                seperator_line = lineno
-                break  # find the first seperator
-
-        if seperator_line != None:
-            name = "\n".join(lines[:seperator_line]).strip()
-            body = "\n".join(lines[seperator_line + 1:])
+    def p_error(self, p):
+        if p:
+            raise TodoSyntaxError(
+                "Character '%s' at line %d" % (p.value[0], p.lineno)
+            )
         else:
-            name = ""
-            body = "\n".join(lines)
+            raise TodoSyntaxError("SyntaxError at EOF")
 
-        self.todo = Todo(name=name)  # reset todo instance
-        return self.parser.parse(body)
+    def p_start(self, p):  # parser start here
+        "start : translation_unit"
+        p[0] = self.todo
+
+    def p_translation_unit(self, p):  # translate all
+        """
+        translation_unit : translation
+                         | translation_unit translation
+                         |
+        """
+        pass
+
+    def p_translation_name(self, p):
+        """
+        translation : NAME
+        """
+        if self.todo.name:
+            raise TodoSyntaxError('Duplicate definition of todo\'s name')
+        self.todo.name = p[1]
+
+    def p_translation_task(self, p):  # translate token TASK
+        """
+        translation : TASK
+        """
+        self.todo.tasks.append(p[1])
 
 
 lexer = Lexer()
-parser = Parser()
+parser = Parser()  # build parser
